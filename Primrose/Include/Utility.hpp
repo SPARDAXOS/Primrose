b856eb5561627bd99050f0b092564d2e7747d3bb
Mohammed Tamim <alexmerceram@hotmail.com>
2023-05-14 14:56:06 +0200

    Added code for parsing shaders

--- Primrose/Include/Utility.hpp
+++ Primrose/Include/Utility.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #include <stdio.h>
+#include <string_view>
+#include <sstream>
+#include <fstream>
 
 
 #define PrintMessage(message) printf("%s \n", message);
+#define PrintMessages(message1, message2) printf("%s %s \n", message1, message2);
 
 
 using uint8 = unsigned char;
@@ -14,3 +18,25 @@ using int8 = signed char;
 using int16 = signed short;
 using int32 = signed int;
 using int64 = signed long long;
+
+
+namespace FileManagement {
+	inline std::string Read(const std::string_view& filePath) {
+		std::stringstream TargetFileData;
+		std::ifstream TargetFile;
+		TargetFile.open(filePath.data(), std::ios::in);
+		if (!TargetFile.is_open()) {
+			PrintMessages("File failed to open! ", filePath.data());
+			return TargetFileData.str();
+		}
+
+		std::string Line;
+		while (getline(TargetFile, Line))
+		{
+			TargetFileData << Line << "\n";
+		}
+
+		TargetFile.close();
+		return TargetFileData.str();
+	}
+}
--- Primrose/Source/Main.cpp
+++ Primrose/Source/Main.cpp
@@ -1,21 +1,25 @@
 #include <Core.hpp>
-
+#include <iostream>
 
 
 
 //System ---> Subsystem --->  Windows (/SUBSYSTEM:WINDOWS) ---> OK!
 //System ---> Subsystem --->  Console (/SUBSYSTEM:CONSOLE) ---> CANT COMPILE!
-int WinMain(int wargc, wchar_t* wargv[]) {
-    std::unique_ptr<Core> Engine = std::make_unique<Core>();
-
-
-    return 0;
-}
+//int WinMain(int wargc, wchar_t* wargv[]) {
+//    std::unique_ptr<Core> Engine = std::make_unique<Core>();
+//
+//    std::stringstream Shader = FileManagement::ReadFromFile("Resources/Shaders/Vertex.Shader");
+//    std::cout << Shader.str();
+//    return 0;
+//}
 
 
 //System ---> Subsystem --->  Windows (/SUBSYSTEM:WINDOWS) ---> CANT COMPILE!
 //System ---> Subsystem --->  Console (/SUBSYSTEM:CONSOLE) ---> OK!
-//int main(int argc, char* argv[]) {
-//
-//    return 0;
-//}
+int main(int argc, char* argv[]) {
+
+    std::string Shader = FileManagement::Read("Resources/Shaders/Vertex.Shader");
+    std::cout << Shader;
+    return 0;
+}   
+
