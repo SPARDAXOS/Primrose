527471806f9f4e57eaad79eb9fcca1a8ba3ac48d
Mohammed <alexmerceram@hotmail.com>
2023-08-02 06:10:17 +0200

    Fixed and finished lighting shader

--- Primrose/Resources/Shaders/Frag_PhongLighting.glsl
+++ Primrose/Resources/Shaders/Frag_PhongLighting.glsl
@@ -19,28 +19,26 @@ uniform vec4 uTint;
 
 void main() {
 	
-
+	//Ambient
 	float AmbientStrength = 0.2;
     vec4 Ambient = vec4(texture(uDiffuseTexUnit, oCoords).xyz * AmbientStrength, 1.0) * uTint; 
 
-	vec3 Normal = oNormalMatrix * normalize(oNormal); //Its this probably. 
+	//Diffuse
+	vec3 Normal = normalize(oNormalMatrix * oNormal);
 	vec3 LightDirection = normalize(uLightPosition - oFragPosition);  
 	float LightDotNormal = max(dot(Normal, LightDirection), 0.0);
-	vec4 Diffuse = texture(uDiffuseTexUnit, oCoords) * LightDotNormal * uLightColor;
+	vec4 Diffuse = texture(uDiffuseTexUnit, oCoords) * LightDotNormal * uLightColor * uTint;
 
-	//Scale affects amount of light
-	//Specular breaks when scaled
-
+	//Specular
 	float SpecularStrength = 0.5;
 	int SpecularShininess = 32;
 	vec3 ViewDirection = normalize(uViewCameraPosition - oFragPosition);
 	vec3 ReflectDirection = reflect(-LightDirection, Normal);
 	float SpecularValue = pow(max(dot(ViewDirection, ReflectDirection), 0.0), SpecularShininess);
-	vec4 Specular = SpecularStrength * SpecularValue * uLightColor;  
+	vec4 Specular = SpecularStrength * SpecularValue * uLightColor * uTint;  
 
 
 	vec4 Results = (Ambient + Diffuse + Specular) * uTint;
-	Results.w = 1.0f;
 
 	FragColor = Results;
 }
